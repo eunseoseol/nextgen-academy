dbe7345a870f763faaf5c11a4d087caae2622bcc
Eunseo Seol <147463468+eunseoseol@users.noreply.github.com>
2025-12-02 13:36:48 +0900

    Update person details in content.tsx

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -2,9 +2,9 @@ import { About, Blog, Gallery, Home, Newsletter, Person, Social, Work } from "@/
 import { Line, Row, Text } from "@once-ui-system/core";
 
 const person: Person = {
-  firstName: "Selene",
-  lastName: "Yu",
-  name: `Selene Yu`,
+  firstName: "Eunseo Seol",
+  lastName: "Seol",
+  name: `Eunseo Seol`,
   role: "Design Engineer",
   avatar: "/images/avatar.jpg",
   email: "example@gmail.com",
